201b63ed4d200dfe4b19f6016c55d9ba0b26c5ac
Kyle Halleman <kylehalleman@users.noreply.github.com>
2020-04-05 22:41:21 -0500

    add back to top and anchor links for headings

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   function addLabelToToc() {
     var toc = document.getElementById("markdown-toc");
 
@@ -20,6 +20,78 @@
     }
   }
 
+  function findTarget(target, selector) {
+    if (target && target !== document) {
+      if (typeof target.matches === "function" && target.matches(selector)) {
+        return target;
+      } else {
+        return findTarget(target.parentNode, selector);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  function delegate(selector, handler) {
+    return function (e) {
+      var target = findTarget(e.target, selector);
+      if (target) {
+        handler.call(target, e);
+      }
+    };
+  }
+
+  function addSectionLinks() {
+    var main = document.querySelector("main");
+    main.addEventListener(
+      "mouseover",
+      delegate("h2, h3, h4, h5, h6", function (e) {
+        var anchor = this.querySelector(".anchor");
+        if (anchor) {
+          anchor.hidden = false;
+        }
+      }),
+      false
+    );
+    main.addEventListener(
+      "mouseout",
+      delegate("h2, h3, h4, h5, h6", function (e) {
+        var anchor = this.querySelector(".anchor");
+        if (anchor) {
+          anchor.hidden = true;
+        }
+      }),
+      false
+    );
+  }
+
+  function addBackToTopLinks() {
+    var main = document.querySelector("main");
+    var headings = main.querySelectorAll("h2, h3, h4, h5, h6");
+    var headingsArray = headings ? Array.prototype.slice.call(headings) : [];
+
+    if (headingsArray.length) {
+      headingsArray.forEach(function (el) {
+        var anchor = document.createElement("a");
+        anchor.className = "anchor";
+        anchor.textContent = "🔗";
+        anchor.href = "#" + el.id;
+        anchor.hidden = true;
+        anchor.setAttribute("aria-hidden", true);
+        el.appendChild(anchor);
+
+        var link = document.createElement("a");
+        link.className = "back-to-top";
+        link.textContent = "Back to top";
+        link.setAttribute("aria-hidden", true);
+        link.href = "#top";
+        el.appendChild(link);
+      });
+    }
+  }
+
   addLabelToToc();
   wrapToc();
+  addSectionLinks();
+  addBackToTopLinks();
 })();
